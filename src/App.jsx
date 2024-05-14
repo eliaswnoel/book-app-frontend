49d8f7b7c4ac12cebb5fde33bbb9a381c494733e
eliaswnoel <eliaswnoel@gmail.com>
2024-05-13 22:36:25 -0400

    nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import "./App.css";
 import { useState } from "react";
-import { Routes, Route, BrowserRouter} from "react-router-dom";
+import { Routes, Route, BrowserRouter, NavLink} from "react-router-dom";
 import Book from "./components/Book";
+import Home from "./components/Home"
 
 function App() {
   return (
     <BrowserRouter> {/* Wrap your entire application with BrowserRouter */}
       <div>
         <nav className="top-nav">
-          <a href="/books">Books</a>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/books">Books</NavLink>
         </nav>
         
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/books" element={<Book />} />
         </Routes>
       </div>
